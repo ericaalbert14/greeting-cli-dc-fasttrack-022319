9bc67c739bd2b7cf846e30323db1b28dc538c67a
Erica Albert <erica.albert@ibm.com>
2019-02-24 15:32:13 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,8 @@
 # code the #greeting method here!
 
 def greeting(name)
-  
+  puts "enter your name"
+  name = gets.chomp
+  puts "Hello, #{name}. It's nice to meet you."
+  puts name.capitalize
+end
